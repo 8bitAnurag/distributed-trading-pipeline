083ccf9e9ed3e337c8796493676416cfc663a831
Anurag <ag01021988@gmail.com>
2026-08-16 22:30:18 +0200

    orderbook logic

--- core/include/trading_limits.h
+++ core/include/trading_limits.h
@@ -16,5 +16,9 @@ namespace core {
     constexpr std::size_t MAX_PRICE_LEVELS = 100'000;
 
     constexpr std::size_t MAX_ORDERS_PER_PRICE_LEVEL = 1024;
+
+    constexpr std::size_t MAX_PRICE_TICKS = 100'000;
+
+    constexpr std::size_t MAX_ACTIVE_ORDERS = 262'144;
 }
 #endif //DISTRIBUTED_TRADING_PIPELINE_LIMITS_H
--- engine/include/order_book.h
+++ engine/include/order_book.h
@@ -6,36 +6,56 @@
 #define DISTRIBUTED_TRADING_PIPELINE_ORDER_BOOK_H
 #include <array>
 #include <cstddef>
+#include <limits>
 #include <cstdint>
 #include "../../core/include/trading_limits.h"
-#include <../../core/include/order.h>
+#include "../../core/include/order.h"
 
 namespace engine {
+    constexpr std::size_t INVALID_INDEX = std::numeric_limits<std::size_t>::max();
 
     struct PriceLevel {
-        uint64_t price{0};
-
-        std::array<core::Order, core::MAX_ORDERS_PER_PRICE_LEVEL> orders{};
-
-        std::size_t order_count{0};
-        std::size_t head{0};
-        std::size_t tail{0};
+        std::size_t head{INVALID_INDEX};
+        std::size_t tail{INVALID_INDEX};
 
         uint64_t total_quantity{0};
+        uint32_t order_count{0};
+    };
+    struct OrderNode {
+        core::Order order{};
+
+        std::size_t next{INVALID_INDEX};
+        std::size_t prev{INVALID_INDEX};
     };
 
     class OrderBook {
     private:
-        std::array<PriceLevel, core::MAX_PRICE_LEVELS> bids_{};
-        std::array<PriceLevel, core::MAX_PRICE_LEVELS> asks_{};
+        static constexpr std::size_t BITMAP_WORDS = (core::MAX_PRICE_TICKS + 63) / 64;
 
-        std::size_t bid_level_count_{0};
-        std::size_t ask_level_count_{0};
+        std::array<PriceLevel, core::MAX_PRICE_TICKS> bids_{};
+        std::array<PriceLevel, core::MAX_PRICE_TICKS> asks_{};
+
+        std::array<OrderNode, core::MAX_ACTIVE_ORDERS> orders_pool_{};
+        std::array<std::size_t, core::MAX_ACTIVE_ORDERS> free_indices_{};
+        std::size_t free_indices_count_{core::MAX_ACTIVE_ORDERS};
+
+        std::array<uint64_t, BITMAP_WORDS> active_bids_{};
+        std::array<uint64_t, BITMAP_WORDS> active_asks_{};
+
+        std::size_t best_bid_index_{INVALID_INDEX};
+        std::size_t best_ask_index_{INVALID_INDEX};
+
+        std::size_t allocate_node();
 
     public:
+        OrderBook() {
+            for (std::size_t i = 0; i < core::MAX_ACTIVE_ORDERS; ++i) {
+                free_indices_[i] = core::MAX_ACTIVE_ORDERS - 1 - i;
+            }
+        }
         bool add_order(const core::Order& order);
         bool cancel_order(uint64_t order_id);
-        
+
         const PriceLevel* best_bid() const; // Highest number
         const PriceLevel* best_ask() const; // Lowest number
     };
